214a353b9884f040fe58fda3f6958cca5ef792c0
James Brook <james.brook@spaaza.com>
2014-08-13 16:24:29 +0200

    Argh! Debugging

--- recipes/php_env_vars.rb
+++ recipes/php_env_vars.rb
@@ -4,20 +4,20 @@ node[:deploy].each do |application, deploy|
   log("application: #{application.to_s}") { level :info }
   log("app envs: #{node[:custom_env][application.to_s]}") { level :info }
 
-  template "#{deploy[:deploy_to]}/current/#{node[:custom_env][application.to_s][:path_to_vars]}/environment_variables.php" do
-    source "environment_variables.php.erb"
-    owner deploy[:user] 
-    group deploy[:group]
-    mode "0666"
-
-    variables( 
-        :env => (node[:custom_env] rescue nil), 
-        :environment => (node[:custom_env][application.to_s][:environment] rescue nil),
-        :application => "#{application}" 
-    )
-
-    only_if do
-     File.directory?("#{deploy[:deploy_to]}/current/#{node[:custom_env][application.to_s][:path_to_vars]}")
-    end
-  end
+  #template "#{deploy[:deploy_to]}/current/#{node[:custom_env][application.to_s][:path_to_vars]}/environment_variables.php" do
+  #  source "environment_variables.php.erb"
+  #  owner deploy[:user] 
+  #  group deploy[:group]
+  #  mode "0666"
+#
+  #  variables( 
+  #      :env => (node[:custom_env] rescue nil), 
+  #      :environment => (node[:custom_env][application.to_s][:environment] rescue nil),
+  #      :application => "#{application}" 
+  #  )
+#
+  #  only_if do
+  #   File.directory?("#{deploy[:deploy_to]}/current/#{node[:custom_env][application.to_s][:path_to_vars]}")
+  #  end
+  #end
 end

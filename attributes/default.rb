3d90cf62ef8dfd2adb34261a257b4cd351afc627
James Brook <james.brook@spaaza.com>
2014-08-13 12:46:58 +0200

    Rely on opsworks default deploy user

--- attributes/default.rb
+++ attributes/default.rb
@@ -41,9 +41,9 @@ default['php-fpm']['pools'] = [
 default['deploy']['test']['application'] = 'test'
 default['deploy']['test']['application_type'] = 'php'
 default['deploy']['test']['deploy_to'] = "/var/www/#{node['deploy']['test']['application']}"
-default['deploy']['test']['user'] = 'www-data'
-default['deploy']['test']['group'] = 'www-data'
-default['deploy']['test']['domains'] = %w(test.dev.com www.test.dev.com backend.test.dev.com)
+#default['deploy']['test']['user'] = 'www-data'
+#default['deploy']['test']['group'] = 'www-data'
+d#efault['deploy']['test']['domains'] = %w(test.dev.com www.test.dev.com backend.test.dev.com)
 default['deploy']['test']['domain_pools'] = { 'www.test.dev.com' => 'www', 'backend.test.dev.com' => 'backend' }
 
 #default['deploy']['test']['home'] = '/tmp/test_deploy_home'
